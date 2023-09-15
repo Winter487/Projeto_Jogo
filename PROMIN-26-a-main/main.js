584d450ff82da2de71d11eebc6223fecf5357656
Winter487 <117038089+Winter487@users.noreply.github.com>
2023-09-15 17:17:04 -0300

    Add files via upload

---
+++ PROMIN-26-a-main/main.js
@@ -0,0 +1,30 @@
+//var platform, platformImg;
+//var player, playerImg;
+//var portal, portalImg;
+var bg, bgImg;
+//var key, keyImg;
+
+function preload(){
+
+    //platformImg = loadImage("");
+    bgImg = loadImage("bg.jpg");
+    //playerImg = loadImage("");
+    //portalImg = loadImage("");
+    //keyImg = loadImage("");
+
+}
+
+function setup(){
+    createCanvas(400, 400);
+
+
+
+
+}
+
+function draw(){
+    background(bgImg);
+
+    drawSprites();
+
+}
